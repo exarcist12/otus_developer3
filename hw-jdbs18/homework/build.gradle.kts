6d66c58455cbee73ce902bc69fed135b9f0f70a6
Stanislav <Stanislav.Petrov@russianpost.ru>
2025-12-15 17:03:33 +0400

    добавлены условия для задачки 18

---
+++ hw-jdbs18/homework/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    implementation(project(":hw-jdbs18:demo"))
+
+    implementation("ch.qos.logback:logback-classic")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.postgresql:postgresql")
+}
